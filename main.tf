bb7895669157b2f6bbd62df4b42367b7bb3c5829
Douglass Coronado <dcoronado@funio.com>
2023-09-22 19:02:20 -0400

    create resources

---
+++ main.tf
@@ -0,0 +1,16 @@
+# since these variables are re-used - a locals block makes this more maintainable
+
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.vnetmcti.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.vnetmcti.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnetmcti.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.vnetmcti.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.vnetmcti.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.vnetmcti.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.vnetmcti.name}-rdrcfg"
+}
+
+resource "azurerm_resource_group" "mcit" {
+  name     = "rg-${var.convention}"
+  location = var.location
+}
